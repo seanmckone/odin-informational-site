c9a150b3afe71eb385749ce51f86ba4c54e57b43
SDMckone <seandmckone@gmail.com>
2024-08-19 13:54:53 -0400

    Finish server-side code

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+const page404 = fs.readFileSync("404.html", "utf-8", (err, data) => {
+  if (err) throw err;
+  return data;
+});
+
+http.createServer(function (req, res) {
+  const q = url.parse(req.url, true);
+  let filename = '';
+  
+  if (q.pathname === '/') {
+    filename = './index.html';
+  } 
+  else {
+    filename = '.' + q.pathname;
+  }
+
+
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      res.write(page404);
+      return res.end();
+    }
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
